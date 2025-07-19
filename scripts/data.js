7cc51724a1a905de23e529ad33d5a10055c043a5
panicintheskies <ynto14@users.noreply.github.com>
2025-07-19 12:33:25 +0700

    Update data.js

--- scripts/data.js
+++ scripts/data.js
@@ -1,5 +1,6 @@
 const PROJECT_DATA = {
   tyeso: ['300x250', '160x600', '300x600', '320x50', '320x100', '728x90', '970x250'],
   tiera: ['300x250'],
-  wanderwords: ['300x250', '1080x1920']
+  wanderwords: ['300x250', '1080x1920'],
+  velviawear: ['300x250']
 };
